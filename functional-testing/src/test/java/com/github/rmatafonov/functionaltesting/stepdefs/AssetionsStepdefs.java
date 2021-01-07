06000c63838e143800ca2295f8a595994b737b08
rmatafonov <matafonov.rv@gmail.com>
2021-01-07 13:54:18 +0300

    Added example of Allure attachment and AssertJ lib

--- functional-testing/src/test/java/com/github/rmatafonov/functionaltesting/stepdefs/AssetionsStepdefs.java
+++ functional-testing/src/test/java/com/github/rmatafonov/functionaltesting/stepdefs/AssetionsStepdefs.java
@@ -2,6 +2,8 @@ package com.github.rmatafonov.functionaltesting.stepdefs;
 
 import com.github.rmatafonov.functionaltesting.services.TestContext;
 import io.cucumber.java.en.Then;
+import io.qameta.allure.Allure;
+import org.assertj.core.api.Assertions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,9 @@ public class AssetionsStepdefs {
 
     @Then("^the var should be fine$")
     public void theVarShouldBeFine() {
-        log.info(testContext.getSomeVar());
+        Allure.addAttachment("Sample attachment", testContext.getSomeVar());
+        Assertions.assertThat(testContext.getSomeVar())
+                .as("Unexpected var value")
+                .isEqualTo("test");
     }
 }
